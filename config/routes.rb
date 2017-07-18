997801e6c5117edbaf7acd1c311770c1d0bd0301
naderae <naderabouezze93@gmail.com>
2017-07-18 16:06:16 -0400

    create route, controller, and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pictures' => 'pictures#index' #take this 'get' request to the 'pictures' controller  'index' action     
 end
