39c832b0141c9edafe0b9fa8ba4178189a292844
naderae <naderabouezze93@gmail.com>
2017-07-24 17:01:05 -0400

    done

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,11 +1,13 @@
 class Picture < ApplicationRecord
 
-def self.older_pictures
-  Picture.where("created_at < ?", Date.current.prev_month)
-end
+  def self.older_pictures
+    Picture.where("created_at < ?", Date.current.prev_month)
+  end
 
-def self.pictures_created_in_year(year)
-  start_date = Date.new(year, 1, 1)
-  end_date = Date.new(year, 12, 31)
-  Picture.where("created_at > ? and created_at <" start_date, end_date)
+  def self.pictures_created_in_year(year)
+    start_date = Date.new(year, 1, 1)
+    end_date = Date.new(year, 12, 31)
+    Picture.where("created_at > ? and created_at < ?", start_date, end_date)
+  end
+
 end
